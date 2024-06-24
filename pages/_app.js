3d0b8cef2217b5110253277c7c2425dac92245af
Cypro Freelance <110410268+Proxyy587@users.noreply.github.com>
2024-06-24 23:05:35 +0530

    font updated

---
+++ pages/_app.js
@@ -0,0 +1,11 @@
+import "@/app/globals.css";
+
+function MyApp({ Component, pageProps }) {
+	return (
+		<main style={{ fontFamily: "Poppins" }}>
+			<Component {...pageProps} />;
+		</main>
+	);
+}
+
+export default MyApp;

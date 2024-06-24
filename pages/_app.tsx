bd7223f45287cbb142c743f0bd38f7e49fde71e2
Dorian Niemiec <dorian.niemiec@svrjs.org>
2024-06-25 00:17:42 +0200

    Change the way of implementing custom font for the server documentation. Also refactor some styles at "app/globals.css" file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,23 @@
-import "@/app/globals.css";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  weight: ["400", "600", "700", "900"],
+  subsets: ["latin"],
+});
 
 function MyApp({ Component, pageProps }) {
 	return (
-		<main style={{ fontFamily: "Poppins" }}>
-			<Component {...pageProps} />
-		</main>
+		<>
+			<style jsx global>{`
+				html {
+					font-family: ${poppins.style.fontFamily};
+				}
+			`}</style>
+			<div className={`antialiased ${poppins.className}`}>
+				<Component {...pageProps} />
+			</div>
+		</>
 	);
 }
 

8580e24b68b64e78f1175af855ed382da184a5e4
Cypro Freelance <110410268+Proxyy587@users.noreply.github.com>
2024-06-26 00:33:15 +0530

    i be real typescript being horny uh

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import { AppProps } from "next/app";
 
 const poppins = Poppins({
 	weight: ["400", "600", "700", "900"],
 	subsets: ["latin"],
 });
 
-function MyApp({ Component, pageProps }: any) {
+function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<>
 			<style jsx global>{`

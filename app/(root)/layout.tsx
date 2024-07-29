8e1b93721a58599ab3e49904ede3086e0c93247b
Dorian Niemiec <dorian.niemiec@svrjs.org>
2024-07-29 20:43:07 +0200

    Replace one <main> tag with <div> tag, and removed a <main> tag, that wrapped prior <main> tag; <main> tag specifies the main content of a document, and there must not be more than one <main> tag!

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -38,10 +38,10 @@ export default function PageLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<main className="flex flex-col min-h-screen">
+		<div className="flex flex-col min-h-screen">
 			<Navbar />
 			<div className="flex-grow flex-1 overflow-x-hidden">{children}</div>
 			<Footer />
-		</main>
+		</div>
 	);
 }

5ddd11ed8441b0f179ed6060abd2199df485dcdf
Dorian Niemiec <dorian.niemiec@svrjs.org>
2024-07-29 21:49:01 +0200

    Fix clipped at the bottom headings with text gradient.

--- components/shared/FAQ.tsx
+++ components/shared/FAQ.tsx
@@ -10,10 +10,10 @@ import {
 const Faq = () => {
   return (
     <section id="faq" className="wrapper container py-24 md:py-28">
-      <h2 className="text-3xl md:text-5xl font-bold mb-4 text-black dark:bg-clip-text dark:text-transparent dark:bg-gradient-to-b dark:from-white dark:to-neutral-400">
+      <h2 className="text-3xl md:text-5xl font-bold mb-4 md:mb-2 md:pb-2 text-black dark:bg-clip-text dark:text-transparent dark:bg-gradient-to-b dark:from-white dark:to-neutral-400">
         Frequently Asked Question
       </h2>
-      <p className="text-lg text-muted-foreground text-start mt-4 mb-8">
+      <p className="text-lg text-muted-foreground text-start mt-4 md:mt-2 mb-8">
         Find answers to common questions about SVRJS
       </p>
       <Accordion

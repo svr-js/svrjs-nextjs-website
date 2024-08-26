be070d9213e0534773d48b4559fcf195873bb3e8
Cypro Freelance <110410268+Proxyy587@users.noreply.github.com>
2024-08-26 17:28:31 +0530

    style changes

--- app/(auth)/_components/Card.tsx
+++ app/(auth)/_components/Card.tsx
@@ -3,21 +3,21 @@ import Link from "next/link";
 import { FC } from "react";
 
 interface CardProps {
-  title: string;
-  url: string;
+	title: string;
+	url: string;
 }
 
 const Card: FC<CardProps> = ({ title, url }) => {
-  return (
-    <div className=" bg-zinc-900 border rounded-lg hover:bg-zinc-800 transition-all">
-      <Link href={url} className="group">
-        <div className="flex-center rounded-lg p-6">
-          <h2 className="text-2xl font-bold mb-2">{title}</h2>
-          <ArrowUpRight className="w-5 h-5 mb-2 ml-2 opacity-0 group-hover:opacity-100 transition-all duration-300" />
-        </div>
-      </Link>
-    </div>
-  );
+	return (
+		<div className="bg-accent border rounded-lg hover:bg-muted transition-all">
+			<Link href={url} className="group">
+				<div className="flex-center rounded-lg p-6">
+					<h2 className="text-2xl font-bold mb-2">{title}</h2>
+					<ArrowUpRight className="w-5 h-5 mb-2 ml-2 opacity-0 group-hover:opacity-100 transition-all duration-300" />
+				</div>
+			</Link>
+		</div>
+	);
 };
 
 export default Card;

f9a172acdbce4261c63dc30b6abea9b6542cc1bc
Cypro Freelance <110410268+Proxyy587@users.noreply.github.com>
2024-08-30 23:06:10 +0530

    updated banner

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,7 @@
 import Footer from "@/components/shared/Footer";
 import Navbar from "@/components/shared/Navbar";
 import Banner from "@/components/widgets/Banner";
+import { Home } from "lucide-react";
 import { Metadata } from "next";
 
 // baseURL [ENV]
@@ -43,10 +44,12 @@ export default function PageLayout({
 		<div className="flex flex-col min-h-screen">
 			{/* Comment or edit this whenever required */}
 			<Banner
+				icon={<Home />}
 				title="SVR.JS 4.0.0 is now on beta!"
 				announcement="The latest beta version is SVR.JS 4.0.0-beta3."
 				link="https://blog.svrjs.org/2024/08/30/SVR-JS-4-0-0-beta3-has-been-released/"
 				buttonText="Read more"
+				className={""}
 			/>
 			<Navbar />
 			<div className="flex-grow flex-1 overflow-x-hidden">{children}</div>

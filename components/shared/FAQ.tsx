d4fd36f05f5b2904d6667fe9fec47ee74931e4d4
Dorian Niemiec <dorian.niemiec@svrjs.org>
2024-09-07 10:57:59 +0200

    fix: replace "Frequently Asked Question" with "Frequently Askes Questions"

--- components/shared/FAQ.tsx
+++ components/shared/FAQ.tsx
@@ -11,7 +11,7 @@ const Faq = () => {
   return (
     <section id="faq" className="wrapper container py-24 md:py-28">
       <h2 className="text-3xl md:text-5xl font-bold mb-4 md:mb-2 md:pb-2 text-black dark:bg-clip-text dark:text-transparent dark:bg-gradient-to-b dark:from-white dark:to-neutral-400">
-        Frequently Asked Question
+        Frequently Asked Questions
       </h2>
       <p className="text-lg text-muted-foreground text-start mt-4 md:mt-2 mb-8">
         Find answers to common questions about SVR.JS

8e9deea0bfe8e4252f027a98a4c81793d551dcd9
Dorian Niemiec <dorian.niemiec@svrjs.org>
2024-09-07 12:19:23 +0200

    fix: add metadata to the newsletter page

--- app/(root)/newsletter/page.tsx
+++ app/(root)/newsletter/page.tsx
@@ -1,5 +1,36 @@
 import Newsletter from "@/components/shared/Newsletter";
 import React from "react";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Newsletter - SVR.JS",
+  description:
+    "Subscribe to our newsletter for updates. We promise no spam emails will be sent.",
+  openGraph: {
+    title: "Newsletter - SVR.JS",
+    description:
+      "Subscribe to our newsletter for updates. We promise no spam emails will be sent.",
+    url: "https://svrjs.org/newsletter",
+    type: "website",
+    images: [
+      {
+        url: "https://svrjs.vercel.app/metadata/svrjs-cover.png",
+        width: 800,
+        height: 600,
+        alt: "Newsletter - SVR.JS"
+      }
+    ]
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@SVR_JS",
+    title: "Newsletter - SVR.JS",
+    description:
+      "Subscribe to our newsletter for updates. We promise no spam emails will be sent.",
+    images: ["https://svrjs.vercel.app/metadata/svrjs-cover.png"],
+    creator: "@SVR_JS"
+  }
+};
 
 const NewsletterPage = () => {
   return <Newsletter />;

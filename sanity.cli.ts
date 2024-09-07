b575dcf42416221e5a86b1efb623752a3ebc17a3
Dorian Niemiec <dorian.niemiec@svrjs.org>
2024-09-07 19:17:30 +0200

    build: load .env file for Sanity CLI configuration

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,4 +1,8 @@
+import { loadEnvConfig } from "@next/env";
 import { defineCliConfig } from "sanity/cli";
+
+const dev = process.env.NODE_ENV !== "production";
+loadEnvConfig(__dirname, dev, { info: () => null, error: console.error });
 
 export default defineCliConfig({
   api: {

96ac8535f883e36d47155171d4b646056291d476
Dorian Niemiec <dorian.niemiec@svrjs.org>
2024-09-07 15:50:01 +0200

    fix: add basePath to Sanity Studio configuration

--- sanity.config.ts
+++ sanity.config.ts
@@ -13,6 +13,8 @@ export default defineConfig({
   projectId: `${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}`,
   dataset: "production",
 
+  basePath: "/studio",
+
   plugins: [structureTool(), visionTool(), codeInput()],
 
   schema: {

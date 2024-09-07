188c5c0f40217c43556e26b6c8fc31055b470e98
Dorian Niemiec <dorian.niemiec@svrjs.org>
2024-09-07 13:13:15 +0200

    fix: add AuthProvider to the login page layout

---
+++ app/login/layout.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import AuthProvider from "@/components/shared/providers/AuthProvider";
+
+export default function AdminLayout({
+  children
+}: {
+  children: React.ReactNode;
+}) {
+  return <AuthProvider>{children}</AuthProvider>;
+}

4001e1b68182c48f0f402b97bee7203545014d1b
Dorian Niemiec <dorian.niemiec@svrjs.org>
2024-09-08 11:39:08 +0200

    fix: remove labels in pagination

--- components/ui/pagination.tsx
+++ components/ui/pagination.tsx
@@ -70,7 +70,6 @@ const PaginationPrevious = ({
     {...props}
   >
     <ChevronLeft className="h-4 w-4" />
-    <span>Previous</span>
   </PaginationLink>
 );
 PaginationPrevious.displayName = "PaginationPrevious";
@@ -85,7 +84,6 @@ const PaginationNext = ({
     className={cn("gap-1 pr-2.5", className)}
     {...props}
   >
-    <span>Next</span>
     <ChevronRight className="h-4 w-4" />
   </PaginationLink>
 );
@@ -101,7 +99,6 @@ const PaginationEllipsis = ({
     {...props}
   >
     <MoreHorizontal className="h-4 w-4" />
-    <span className="sr-only">More pages</span>
   </span>
 );
 PaginationEllipsis.displayName = "PaginationEllipsis";

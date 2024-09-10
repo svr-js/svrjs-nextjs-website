725912bbe67bfb1f3605baf87a72c7e23a9728b4
Dorian Niemiec <dorian.niemiec@svrjs.org>
2024-09-10 19:52:24 +0200

    fix: set the language code attribute in <html> tag to "en-US" for both app router and pages router

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,7 +46,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en-US" suppressHydrationWarning>
       <body className={`antialiased ${poppins.className}`}>
         <ThemeProvider
           attribute="class"

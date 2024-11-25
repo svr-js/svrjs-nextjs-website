869833b34abec9885c22511b46a1a83fdbc0cf12
Dorian Niemiec <dorian.niemiec@svrjs.org>
2024-11-25 06:31:28 +0100

    fix: don't wrap Statistics component contents in <section> element

--- components/shared/Statistics.tsx
+++ components/shared/Statistics.tsx
@@ -3,18 +3,16 @@ import NumberTicker from "../widgets/num-tick";
 
 const Statistics = () => {
   return (
-    <section>
-      <div className="grid grid-cols-2 lg:grid-cols-4 gap-8">
-        {stats.map(({ title, count }) => (
-          <div key={title} className="space-y-2 text-center">
-            <p className="text-3xl sm:text-4xl font-bold">
-              <NumberTicker value={count} />+
-            </p>
-            <p className="text-xl text-muted-foreground">{title}</p>
-          </div>
-        ))}
-      </div>
-    </section>
+    <div className="grid grid-cols-2 lg:grid-cols-4 gap-8">
+      {stats.map(({ title, count }) => (
+        <div key={title} className="space-y-2 text-center">
+          <p className="text-3xl sm:text-4xl font-bold">
+            <NumberTicker value={count} />+
+          </p>
+          <p className="text-xl text-muted-foreground">{title}</p>
+        </div>
+      ))}
+    </div>
   );
 };
 

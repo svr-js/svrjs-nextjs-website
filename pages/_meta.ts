00ed127dde6d0802c1e1dbd9276f4264fe60ed34
Dorian Niemiec <dorian.niemiec@svrjs.org>
2024-11-28 19:04:58 +0100

    fix: add the navbar links to the documentation page

--- pages/_meta.ts
+++ pages/_meta.ts
@@ -1,6 +1,21 @@
 export default {
+  index: {
+    title: "Home",
+    href: "/",
+    type: "page"
+  },
   docs: {
-    title: "SVR.JS documentation",
+    title: "Docs",
+    type: "page"
+  },
+  blog: {
+    title: "Blog",
+    href: "/blog",
+    type: "page"
+  },
+  forum: {
+    title: "Forum",
+    href: "https://forum.svrjs.org",
     type: "page"
   }
 };
